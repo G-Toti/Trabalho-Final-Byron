68fe4efc115f4e0f031a8890a3c663b90b533689
Gabriel Toti <gabrielsilva@byronsolutions.com>
2024-02-27 13:16:54 -0300

    Adicionando header ao site.

---
+++ trabalho-final-front/components/Header.tsx
@@ -0,0 +1,56 @@
+"use client";
+import React from "react";
+import { useScrollContext } from "./ScrollContext";
+
+const Header = () => {
+  const context = useScrollContext(); // pega o contexto
+
+  const curSection = context?.curSection; // passa o valor do contexto pra uma variavel
+
+  const highlight = "font-bold"; // define como vai funcionar o highlight
+
+  return (
+    <header className="fixed right-0 left-0 bg-gray-dark text-gray-base font-inter shadow-gray-600 shadow-md">
+      <div className="flex items-center justify-around gap-20 xl:gap-40 my-0 mx-auto lg:h-28">
+        <div className="flex items-center justify-center lg:justify-start gap-3 w-fit my-4 p-4 md:p-0">
+          <picture className="flex justify-center w-1/6">
+            <img src="images/logo_place_holder.png" />
+          </picture>
+          <h1 className="uppercase text-xl md:text-2xl filter drop-shadow-2xl">
+            Campeonato Brasileiro
+          </h1>
+        </div>
+        <nav className="hidden lg:block">
+          <ul className="flex items-center gap-5 xl:gap-6 text-base lg:text-lg">
+            <li>
+              <a
+                href="#rodadas"
+                className={curSection === "rodadas" ? highlight : ""}
+              >
+                Rodadas da semana
+              </a>
+            </li>
+            <li>
+              <a
+                href="#tabela"
+                className={curSection === "tabela" ? highlight : ""}
+              >
+                Tabela do Campeonato
+              </a>
+            </li>
+            <li>
+              <a
+                href="#noticias"
+                className={curSection === "noticias" ? highlight : ""}
+              >
+                Notícias
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- trabalho-final-front/src/app/layout.tsx
+++ trabalho-final-front/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Header from "../../components/Header";
 import "./globals.css";
+import ScrollContext from "../../components/ScrollContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ScrollContext>
+          <Header />
+        </ScrollContext>
+
+        <main className="font-inter">{children}</main>
+      </body>
     </html>
   );
 }
